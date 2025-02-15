875e84ece665e2714c163ff5026db8aae8868741
Harsh Userethe <82079150+HarshUserethe@users.noreply.github.com>
2025-02-15 23:48:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,5 +37,5 @@ app.get('/ping', (req, res) => {
   res.send("Welcome to server");
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
